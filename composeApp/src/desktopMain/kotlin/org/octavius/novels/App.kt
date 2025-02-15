664c064f8d52222d70ea13765060dd84ef31e2b1
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2025-02-15 22:52:50 +0100

    init

---
+++ composeApp/src/desktopMain/kotlin/org/octavius/novels/App.kt
@@ -0,0 +1,13 @@
+package org.octavius.novels
+
+import androidx.compose.runtime.*
+import org.octavius.novels.navigator.Navigator
+import org.octavius.novels.state.LocalState
+import org.octavius.novels.state.State
+import org.octavius.novels.theme.NovelsTheme
+import org.octavius.screens.MainScreen
+
+
+@Composable
+fun App() {
+}
