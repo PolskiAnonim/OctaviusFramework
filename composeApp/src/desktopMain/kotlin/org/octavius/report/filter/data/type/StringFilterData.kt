c28ebc66696136f6f5d448f4b4d3a3630535b6c7
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2025-07-13 00:58:44 +0200

    feat: immutable filter data

--- composeApp/src/desktopMain/kotlin/org/octavius/report/filter/data/type/StringFilterData.kt
+++ composeApp/src/desktopMain/kotlin/org/octavius/report/filter/data/type/StringFilterData.kt
@@ -1,50 +1,51 @@
 package org.octavius.report.filter.data.type
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.buildJsonObject
-import kotlinx.serialization.json.jsonPrimitive
-import kotlinx.serialization.json.put
+import kotlinx.serialization.json.*
 import org.octavius.report.FilterMode
 import org.octavius.report.NullHandling
 import org.octavius.report.StringFilterDataType
 import org.octavius.report.filter.data.FilterData
 
-class StringFilterData: FilterData() {
-    val filterType: MutableState<StringFilterDataType> = mutableStateOf(StringFilterDataType.Contains)
-    val value: MutableState<String> = mutableStateOf("")
-    val caseSensitive: MutableState<Boolean> = mutableStateOf(false)
+data class StringFilterData(
+    override val mode: FilterMode = FilterMode.Single,
+    override val nullHandling: NullHandling = NullHandling.Ignore,
+    val filterType: StringFilterDataType = StringFilterDataType.Contains,
+    val value: String = "",
+    val caseSensitive: Boolean = false
+) : FilterData {
 
-    override fun resetValue() {
-        filterType.value = StringFilterDataType.Contains
-        value.value = ""
-        caseSensitive.value = false
-    }
 
     override fun isActive(): Boolean {
-        return value.value.trim().isNotEmpty() || nullHandling.value != NullHandling.Ignore
-    }
-
-    override fun getTrackableStates(): List<Any?> {
-        return listOf(filterType.value, value.value, caseSensitive.value, nullHandling.value, mode.value)
+        return value.trim().isNotEmpty() || nullHandling != NullHandling.Ignore
     }
 
     override fun serialize(): JsonObject {
         return buildJsonObject {
-            put("filterType", filterType.value.name)
-            put("value", value.value)
-            put("caseSensitive", caseSensitive.value)
-            put("nullHandling", nullHandling.value.name)
-            put("mode", mode.value.name)
+            put("filterType", filterType.name)
+            put("value", value)
+            put("caseSensitive", caseSensitive)
+            put("nullHandling", nullHandling.name)
+            put("mode", mode.name)
         }
     }
 
-    override fun deserialize(data: JsonObject) {
-        resetFilterData()
-        filterType.value = data["filterType"]!!.jsonPrimitive.content.let { StringFilterDataType.valueOf(it) }
-        value.value = data["value"]!!.jsonPrimitive.content
-        nullHandling.value = data["nullHandling"]!!.jsonPrimitive.content.let { NullHandling.valueOf(it) }
-        mode.value = data["mode"]!!.jsonPrimitive.content.let { FilterMode.valueOf(it) }
+    companion object {
+        fun deserialize(data: JsonObject): StringFilterData {
+            return StringFilterData(
+                filterType = data["filterType"]!!.jsonPrimitive.content.let { StringFilterDataType.valueOf(it) },
+                value = data["value"]!!.jsonPrimitive.content,
+                caseSensitive = data["caseSensitive"]!!.jsonPrimitive.boolean,
+                nullHandling = data["nullHandling"]!!.jsonPrimitive.content.let { NullHandling.valueOf(it) },
+                mode = data["mode"]!!.jsonPrimitive.content.let { FilterMode.valueOf(it) }
+            )
+        }
+    }
+
+    override fun withMode(newMode: FilterMode): FilterData {
+        return this.copy(mode = newMode)
+    }
+
+    override fun withNullHandling(newNullHandling: NullHandling): FilterData {
+        return this.copy(nullHandling = newNullHandling)
     }
 }
