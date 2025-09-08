5b846cfdbc23e6765dd9b5a3c8fa9650933bebb2
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2025-09-08 18:21:22 +0200

    fix: popup

--- extension-popup/src/jsMain/kotlin/org/octavius/extension/app/Main.kt
+++ extension-popup/src/jsMain/kotlin/org/octavius/extension/app/Main.kt
@@ -1,17 +1,12 @@
 package org.octavius.extension.app
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.CanvasBasedWindow
-import org.octavius.ui.theme.AppTheme
+import androidx.compose.ui.window.ComposeViewport
 
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasBasedWindow(canvasElementId = "root", title = "Octavius Helper") {
-                App()
+    ComposeViewport {
+        App()
     }
 }
