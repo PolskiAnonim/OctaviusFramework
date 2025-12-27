cb26f46fb057e9e1c202af7dbf1c0d678bb4a30c
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2025-12-27 10:31:36 +0100

    feat: QueryFragment with postfix and prefix and addParenthesis

--- database/api/src/desktopMain/kotlin/org/octavius/data/QueryFragment.kt
+++ database/api/src/desktopMain/kotlin/org/octavius/data/QueryFragment.kt
@@ -16,23 +16,31 @@ data class QueryFragment(
 )
 
 /**
- * Łączy listę fragmentów w jeden, używając podanego separatora.
+ * Łączy listę fragmentów w jeden, działając analogicznie do standardowego [joinToString],
+ * ale z uwzględnieniem scalania parametrów.
+ *
+ * Postfix i prefix mają zastosowanie głównie dla [org.octavius.data.builder.RawQueryBuilder]. Preferowanym sposobem dodawania samych klauzul
+ * jest użycie odpowiednich builderów
  *
  * 1.  **Bezpiecznie łączy mapy parametrów**, rzucając błąd w przypadku konfliktu kluczy.
- *     To zapobiega cichemu nadpisywaniu parametrów.
- * 2.  Ignoruje puste fragmenty, upraszczając logikę w miejscu wywołania.
- * 3.  Dla pewności otacza każdy fragment nawiasami, co gwarantuje poprawną kolejność operacji.
+ * 2.  Ignoruje puste fragmenty. Jeśli po filtrowaniu lista jest pusta, zwraca pusty QueryFragment
+ *     (ignorując prefix i postfix, aby nie generować np. pustego "WHERE ()").
+ * 3.  Otacza każdy *pojedynczy* fragment nawiasami dla zachowania priorytetów operatorów.
  *
- * @param separator Ciąg znaków używany do oddzielenia części SQL, np. " AND ".
+ * @param separator Ciąg znaków oddzielający fragmenty, np. " AND ".
+ * @param prefix Ciąg wstawiany na początku całego wynikowego łańcucha (np. "WHERE ").
+ * @param postfix Ciąg wstawiany na końcu całego wynikowego łańcucha (np. ";").
+ * @param addParenthesis Czy dodawać nawiasy wokół fragmentów - domyślnie true
+ *
  * @return Nowy, połączony [QueryFragment].
  */
-fun List<QueryFragment>.join(separator: String): QueryFragment {
+fun List<QueryFragment>.join(separator: String, prefix: String = "", postfix: String = "", addParenthesis: Boolean = true): QueryFragment {
     val significantFragments = this.filter { it.sql.isNotBlank() }
+    // Jeśli nie ma znaczących fragmentów, zwracamy pusty obiekt.
     if (significantFragments.isEmpty()) {
         return QueryFragment("")
     }
-
-    val finalSql = significantFragments.joinToString(separator) { "(${it.sql})" }
+    val finalSql = significantFragments.joinToString(separator, prefix, postfix) { if (addParenthesis) "(${it.sql})" else it.sql }
 
     val finalParams = mutableMapOf<String, Any?>()
     significantFragments.forEach { fragment ->
